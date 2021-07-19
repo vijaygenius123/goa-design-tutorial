ff715519eff498af0db10fb9fa9f15fb394eee34
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-07-19 22:00:32 +0530

    (docs) Basic C4 Example

---
+++ design/model.go
@@ -0,0 +1,34 @@
+package design
+
+import . "goa.design/model/dsl"
+
+var _ = Design("Getting Started", "This is a modol of my software system", func() {
+
+	var System = SoftwareSystem("Software System", "My Software System", func(){
+		Tag("system")
+	})
+
+	Person("User", "A user of my software system", func() {
+		Uses(System, "Uses")
+		Tag("person")
+	})
+
+	Views(func(){
+		SystemContextView(System, "SystemContext", "An example of a System Context diagram", func(){
+			AddAll()
+			AutoLayout(RankLeftRight)
+		})
+		Styles(func(){
+			ElementStyle("system", func(){
+				Background("#1168bd")
+				Color("#ffffff")
+			})
+			ElementStyle("person", func(){
+				Shape(ShapePerson)
+				Background("#08427b")
+				Color("#ffffff")
+			})
+		})
+	})
+
+})
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module calc
 
 go 1.14
 
-require goa.design/goa/v3 v3.4.3 // indirect
+require (
+	goa.design/goa/v3 v3.4.3 // indirect
+	goa.design/model v1.7.6 // indirect
+)
